6fdfff5ea5842997a04849aa52aa410ca022edb7
ryosuke.murao <ryouke.murao@rakus.co.jp>
2017-06-14 11:48:54 +0900

    Speed追加

--- src/main/java/com/example/demo/Carcar.java
+++ src/main/java/com/example/demo/Carcar.java
@@ -1,5 +1,14 @@
 package com.example.demo;
 
 public class Carcar {
+	private int speed;
 
+	public int getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+	
 }
